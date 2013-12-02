9056f3749108b5802fa10a5e0a18d43ae52e3712
indigo0086 <indigo0086@gmail.com>
2013-12-02 17:02:47 -0500

    Removing common from /app

--- client/src/app/common/authenticationService.js
+++
@@ -1,9 +0,0 @@
-angular.module("authentication")
-    .factory("AuthenticationService", function(){
-        var data = {
-            isAuthenticated: false,
-            username: ""
-        }
-
-        return data
-    });
